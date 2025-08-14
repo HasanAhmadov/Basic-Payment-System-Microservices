5647cdea48bc206391eb060c52d180843c9f1fdb
HasanAhmadov <160146662+HasanAhmadov@users.noreply.github.com>
2025-08-14 09:34:11 +0400

    Architecture change made and initial commit

---
+++ OrderService/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("GetHello")]
+        public string Get()
+        {
+            return "Hello from Order Service!";
+        }
+    }
+}
